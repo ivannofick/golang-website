d7e0f1ef8dd68faecb31c7c841ca6a7df70610f7
ivannofick <ivannofick@gmail.com>
2021-05-05 00:40:33 +0700

    learn get query url

--- src/config/database.go
+++ src/config/database.go
@@ -1,8 +1,21 @@
 package config
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 func HandlerProduct(writer http.ResponseWriter, r *http.Request) {
-	writer.Write([]byte("Product Page"))
+	id := r.URL.Query().Get("id")
+	idNum, err := strconv.Atoi(id)
+	if err != nil || idNum < 1 {
+		http.NotFound(writer, r)
+		return
+	}
+
+	fmt.Fprintf(writer, "Product Page : %d", idNum)
+
+	// writer.Write([]byte("Product Page"))
 
 }
