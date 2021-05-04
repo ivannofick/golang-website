b65a8a8a78412655c3525b00302694062adb5121
ivannofick <ivannofick@gmail.com>
2021-05-05 00:58:02 +0700

    [bug] handler

---
+++ src/config/handler.go
@@ -0,0 +1,66 @@
+package config
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"path"
+	"strconv"
+)
+
+func HandlerIndex(writer http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" { // ini untuk cek, jika path / url gak ditemukan
+		http.NotFound(writer, r)
+		return
+	}
+	var filePath = path.Join("view", "index.html") // folder tidak boleh huruf besar...!
+	var tmpl, err = template.ParseFiles(filePath)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var data = map[string]interface{}{
+		"title": "Website Golang",
+		"name":  "saya",
+	}
+
+	err = tmpl.Execute(writer, data)
+	if err != nil {
+		InternalServerError(writer)
+	}
+}
+
+func HandlerAbout(writer http.ResponseWriter, r *http.Request) {
+	writer.Write([]byte("About Page"))
+}
+
+func HandlerProductDetail(writer http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	idNum, err := strconv.Atoi(id)
+	if err != nil || idNum < 1 {
+		http.NotFound(writer, r)
+		return
+	}
+
+	fmt.Fprintf(writer, "Product Page : %d", idNum)
+}
+
+func HandlerProduct(writer http.ResponseWriter, r *http.Request) {
+	var filePath = path.Join("view", "index.html") // folder tidak boleh huruf besar...!
+	var tmpl, err = template.ParseFiles(filePath)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var data = map[string]interface{}{
+		"title": "Website Golang",
+		"name":  "saya",
+	}
+
+	err = tmpl.Execute(writer, data)
+	if err != nil {
+		InternalServerError(writer)
+	}
+}
